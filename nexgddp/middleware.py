be1bd9b24ab9e8e114b3850e0a18fa948003c558
Tiago Garcia <tiagojsag@gmail.com>
2019-10-03 14:03:13 +0200

    Add basic test strucutre and dataset DELETE endpoint

--- nexgddp/middleware.py
+++ nexgddp/middleware.py
@@ -237,3 +237,17 @@ def is_microservice(func):
             return error(status=403, detail="Not authorized")
 
     return wrapper
+
+
+def is_microservice_or_admin(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.debug("Checking microservice user")
+        logged_user = json.loads(request.args.get("loggedUser", None))
+        if (logged_user.get("id") == "microservice") or (logged_user.get("role") == "ADMIN"):
+            logging.debug("is microservice or admin");
+            return func(*args, **kwargs)
+        else:
+            return error(status=403, detail="Not authorized")
+
+    return wrapper
